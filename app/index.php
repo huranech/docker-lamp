7b7b8ce4f0f9cf5c385ef583040432b7bd24ae02
huranech <hugo.ran142@gmail.com>
2022-10-05 19:33:49 +0200

    hola hola

--- app/index.php
+++ app/index.php
@@ -1,29 +1,44 @@
-<?php
-  echo '<h1>Yeah, it works!<h1>';
-  // phpinfo();
-  $hostname = "db";
-  $username = "admin";
-  $password = "test";
-  $db = "database";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Catálogo de plantas</title>
+</head>
+<body>
 
-  $conn = mysqli_connect($hostname,$username,$password,$db);
-  if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+  <div>Hola hola, soy maire figueroa</div>
+  <?php
+    echo '<h1>Yeah, it works!<h1>';
+    // phpinfo();
+    $hostname = "db";
+    $username = "admin";
+    $password = "test";
+    $db = "database";
+
+    $conn = mysqli_connect($hostname,$username,$password,$db);
+    if ($conn->connect_error) {
+      die("Database connection failed: " . $conn->connect_error);
+    }
+
+
+
+  $query = mysqli_query($conn, "SELECT * FROM usuarios")
+    or die (mysqli_error($conn));
+
+  while ($row = mysqli_fetch_array($query)) {
+    echo
+    "<tr>
+      <td>{$row['id']}</td>
+      <td>{$row['nombre']}</td>
+    </tr>";
+    
+
   }
 
+  ?>
+</body>
+</html>
 
 
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['id']}</td>
-    <td>{$row['nombre']}</td>
-   </tr>";
-   
-
-}
-
-?>
