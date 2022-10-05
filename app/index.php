8ed648f5f1403d1e18b8c77a21c4379fe99818cb
huranech <hugo.ran142@gmail.com>
2022-10-05 20:36:30 +0200

    barra de navegación decente

--- app/index.php
+++ app/index.php
@@ -5,10 +5,33 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Catálogo de plantas</title>
+  <link rel="stylesheet" type="text/css" href="/Escritorio/Proyecto_lgssi/docker-lamp/app/estilos.css">
+  
+  <style>
+    nav ul{list-style-type:none;
+    text-align:center;}
+
+    nav ul li{
+    display: inline;
+    text-align: center;
+    margin: 0 10px 0 0;}
+
+    nav ul li a{
+    padding: 2px 7px 2px 7px;
+    color: #666;
+    background-color: #eeeeee;
+    border: 1px solid #ccc;
+    text-decoration: none;}
+
+    nav ul li a:hover{
+    background-color: #333333;
+    color: #ffffff;} 
+  </style>
+
 </head>
 <body>
-  <nav>
-    <ul>
+  <nav id="navbar">
+    <ul class="navlist">
       <li><a href="#Catalogo de plantas">Catálogo de plantas</a></li>
       <li><a href="#Modificar catálogo">Modificar catálogo</a></li>
       <li><a href="#Iniciar sesiónc">Iniciar sesión</a></li>
