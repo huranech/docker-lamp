a13d053adbc980cae45afdd7592eb3bef27e0ff1
huranech <hugo.ran142@gmail.com>
2022-10-09 12:13:41 +0200

    registrarse

--- app/registrarse.php
+++ app/registrarse.php
@@ -7,6 +7,26 @@
     <title>Document</title>
 </head>
 <body>
-    Aquí se registra
+    <div>
+        <form>
+            <h1>Regístrate</h1>
+            <label for="nombre y apellidos"><b>Nombre y apellidos:</b>
+            <input type="text" name="nombre y apellidos" required>
+
+            <label for="dni"> <b>DNI:</b>
+            <input type="text" name="dni" required>
+
+            <label for="teléfono"> <b>Teléfono:</b>
+            <input type="text" name="teléfono" required>
+
+            <label for="fecha de nacimiento"> <b>Fecha de nacimiento:</b>
+            <input type="text" name="fecha de nacimiento" required>
+
+            <label for="email"> <b>Email:</b>
+            <input type="email" name="email" required>
+
+            <input type="submit" name="create" value="Registrarse">
+    </form>
+</div>
 </body>
 </html>
