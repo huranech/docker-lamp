34dec0e1620d41ce67bb6dc4bf212f231afb0330
huranech <hugo.ran142@gmail.com>
2022-10-10 17:44:24 +0200

    1

--- app/procesar-registrarse.php
+++ app/procesar-registrarse.php
@@ -1,9 +1,5 @@
 <?php
 
-if (dni($_POST["DNI"])){
-    die("El DNI introducido no es válido");
-}
-
 //conectar con la base de datos
 $hostname = "db";
 $username = "admin";
@@ -27,16 +23,3 @@ if($resultado)
 else{
     echo 'no se ha podido guardar';
 }
-
-//funciones creadas
-function dni($dni){
-    $letra = substr($dni, -1);
-    $numeros = substr($dni, 0, -1);
-    $valido;
-    if (substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros%23, 1) == $letra && strlen($letra) == 1 && strlen ($numeros) == 8 ){
-      $valido=true;
-    }else{
-      $valido=false;
-    }
-    return $valido;
-  }
