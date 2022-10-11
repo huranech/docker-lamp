5db07a5cd18c3a20a10ff2f177346e882490973d
PaulaPinto001 <ppinto001@ikasle.ehu.eus>
2022-10-11 15:08:09 +0200

    P

---
+++ app/procesar-perfil.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_POST["submit"])) {
+  
+ $nombre = $_POST["nombre"];
+ $usuario = $_POST["usuario"];
+ $contrasena = $_POST["contrasena"];
+ $dni = $_POST["dni"];
+ $telefono = $_POST["telefono"];
+ $fechanato = $_POST["fechanato"];
+ $email = $_POST["email"];
+
+ require_once 'index.php';
+ require_once 'funciones.php';
+
+ $id = random_num(20);
+ $sql = "ALTER TABLE usuarios WHERE usuario = '$usuario SET usuarios(id, nombre, usuario, contrasena, DNI, telefono, fechanato, email) VALUES ('$id', '$nombre', '$usuario', '$contrasena', '$dni', '$telefono', '$fechanato', '$email');";
+ mysqli_query($conn, $sql);
+
+}
