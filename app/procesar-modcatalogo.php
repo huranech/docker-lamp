d69170303b348b99a6d0d0b59c83279fbde5b8d1
huranech <hugo.ran142@gmail.com>
2022-10-12 20:16:09 +0200

    modificar planta

--- app/procesar-modcatalogo.php
+++ app/procesar-modcatalogo.php
@@ -17,6 +17,23 @@ if(isset($_POST["anadir"])) {
 
 }
 
+if(isset($_POST["modificar"])) {
+  
+    $nomcomunant = $_POST["nomcomunant"];
+    $nomcomun = $_POST["nomcomun"];
+    $nomcient = $_POST["nomcient"];
+    $origen = $_POST["origen"];
+    $descripcion = $_POST["descripcion"];
+    $temporada = $_POST["temporada"];
+   
+    require_once 'index.php';
+    require_once 'funciones.php';
+   
+    $sql = "UPDATE plantas SET nomcomun = '$nomcomun', nomcient = '$nomcient', origen = '$origen', descripcion = '$descripcion', temporada = '$temporada' WHERE nomcomun = '$nomcomunant';";
+    mysqli_query($conn, $sql);
+   
+   }
+
 if(isset($_POST["eliminar"])) {
   
     $nomcomun = $_POST["nomcomun"];
@@ -26,4 +43,4 @@ if(isset($_POST["eliminar"])) {
    
     $sql = "DELETE FROM plantas WHERE nomcomun = '$nomcomun';";
     mysqli_query($conn, $sql);
-   }
+}
