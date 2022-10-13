612dadd950d96d840a0333f1ab88cc954dd00fbb
PaulaPinto001 <ppinto001@ikasle.ehu.eus>
2022-10-13 11:42:37 +0200

    ff

--- app/catalogoplantas.php
+++ app/catalogoplantas.php
@@ -10,6 +10,31 @@
 <body>
     <div>
         <h1>Catalogo de plantas</h1>
+
+<?php
+$nombre_bd = 'database.sql';
+
+if (!mysqli_connect('localhost', 'admin', 'test')) {
+    echo 'No se pudo conectar a mysql';
+    exit;
+}
+
+$sql = "SHOW TABLES FROM $nombre_bd";
+$resultado = mysql_query($sql);
+
+//if (!$resultado) {
+    //echo "Error de BD, no se pudieron listar las tablas\n";
+    //echo 'Error MySQL: ' . mysql_error();
+    //exit;
+//}
+
+while ($fila = mysql_fetch_row($resultado)) {
+    echo "Tabla: {$fila[0]}\n";
+}
+
+mysql_free_result($resultado);
+
+?>
     </div>
 </body>
 </html>
