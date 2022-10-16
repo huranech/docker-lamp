938d6a625faceb28e181a734194429df95bde553
huranech <hugo.ran142@gmail.com>
2022-10-16 19:48:36 +0200

    últimos cambios

--- app/catalogoplantas.php
+++ app/catalogoplantas.php
@@ -38,8 +38,6 @@
           <li><a href="perfil.php">Perfil</a></li>
         </ul>
     </nav>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
     
     <div class="container">
         <br>
