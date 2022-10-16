c678aeeb77adea1467d00e5e7da4ced386534ffd
huranech <hugo.ran142@gmail.com>
2022-10-16 16:24:16 +0200

    cambios

--- app/borrar.php
+++ app/borrar.php
@@ -4,6 +4,6 @@ require_once('conexion.php');
 if(isset($_GET['id']))
 {
     $id = $_GET['id'];
-    $sql = "DELETE FROM plantas  WHERE id = $id;";
+    $sql = "DELETE FROM plantas WHERE id = $id;";
     mysqli_query($conn, $sql);
 }
