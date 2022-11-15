131056e4c5efe6a173f0bda757ae82a8e410a86d
huranech <hugo.ran142@gmail.com>
2022-11-15 20:58:19 +0100

    x

--- app/perfil.php
+++ app/perfil.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta
   http-equiv="Content-Security-Policy"
-  content="style-src 'self'" />
+  content="style-src 'self';" />
     <title>Document</title>
 
     <style>
