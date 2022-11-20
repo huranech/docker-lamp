9cec8096e5a0d3ac8e4eb93c0085da3b42a2e5e9
Foxo10 <oiertxu.ten@gmail.com>
2022-11-20 21:46:07 +0100

    sdfasdf

--- app/csrf.php
+++ app/csrf.php
@@ -1,14 +1,30 @@
 <?php
 
 session_start();
-
-function csrf(){
-    if(function_exists('mcryp_create_iv')) {
-        $_SESSION['token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_RANDOM));
-
-    } else {
-        $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
+if(isset($_POST) & !empty($_POST)){
+    //Validacion token CSRF
+    if($_POST['csrf_token']== $_SESSION['csrf_token']){
+        echo "token CSRF validado";
+    } else{
+        echo "Problemas de validacion con el token CSRF";
     }
 }
-return ($_SESSION['token']);
-?>
+// tiempo de validacion
+$max_time = 60*60*24;
+if(isset($_SESSION['csrf_token_time'])){
+    $token_time = $_SESSION['csrf_token_time'];
+    if(($token_time + $max_time) >= time()){
+        unset($_SESSION)['csrf_token'];
+        unset($_SESSION)['csrf_token_time'];
+    }
+}
+    
+
+//1. crear token
+$token = md5(uniqid(rand(), true));
+$_SESSION['csrf_token'] = $token;
+$_SESSION['csrf_token_time'] = time();
+//2. añadir token al formulario
+//<input type="hidden" name="csrf_token" value="<?php echo $token;
+
+?> 
