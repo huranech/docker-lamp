312725dcbe641d35a25661304e8748a290453534
Foxo10 <oiertxu.ten@gmail.com>
2022-11-20 22:43:59 +0100

    login

--- app/login.php
+++ app/login.php
@@ -2,7 +2,8 @@
 header("X-Frame-Options: SAMEORIGIN");
 header("X-Content-Type-Options: nosniff");
 session_start();
-$_SESSION["token"] = bin2hex(random_bytes(32));
+require "./csrf.php"
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- app/procesar-login.php
+++ app/procesar-login.php
@@ -1,46 +1,39 @@
 <?php
 session_start();
 header("Location: ../index.php");
-//comprobar que el token esté establecido
-if(!empty($_POST['token']) || !isset($_SESSION["token"])){
-    exit("Token sin activar");
-}
-//comprobar validacion
+require "./csrf.php";
+if(CSRF::validate($_POST['token'])){
+    $usuario = $_POST["usuario"];
+    $contrasena = $_POST["contrasena"];
 
-    if(isset($_POST["submit"])) {
-        if($_POST["token"] == $_SESSION["token"]){
-        
-            $usuario = $_POST["usuario"];
-            $contrasena = $_POST["contrasena"];
-        
-            require_once 'index.php';
-            require_once 'funciones.php';
+    require_once 'index.php';
+    require_once 'funciones.php';
 
-            $stmt = $conn->prepare("SELECT * FROM usuarios WHERE usuario = ? limit 1;");
-            $stmt->bind_param('s', $usuario);
-            $stmt->execute();
-            $resultado = $stmt->get_result();
-            
+    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE usuario = ? limit 1;");
+    $stmt->bind_param('s', $usuario);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    
 
-            if($resultado)
+    if($resultado)
+    {
+        if($resultado && mysqli_num_rows($resultado) > 0)
+        {
+            $user_data = $resultado->fetch_assoc();
+
+            if(password_verify($contrasena, $user_data['contrasena']))
             {
-                if($resultado && mysqli_num_rows($resultado) > 0)
-                {
-                    $user_data = $resultado->fetch_assoc();
-
-                    if(password_verify($contrasena, $user_data['contrasena']))
-                    {
-                        $_SESSION['id_usuario'] = $user_data['id'];
-                        $_SESSION['nombre'] = $user_data['nombre'];
-                        $_SESSION['usuario'] = $user_data['usuario'];
-                        $_SESSION['contrasena'] = $contrasena;
-                        $_SESSION['DNI'] = $user_data['DNI'];
-                        $_SESSION['telefono'] = $user_data['telefono'];
-                        $_SESSION['fechanato'] = $user_data['fechanato'];
-                        $_SESSION['email'] = $user_data['email'];
-                    }
-                }
+                $_SESSION['id_usuario'] = $user_data['id'];
+                $_SESSION['nombre'] = $user_data['nombre'];
+                $_SESSION['usuario'] = $user_data['usuario'];
+                $_SESSION['contrasena'] = $contrasena;
+                $_SESSION['DNI'] = $user_data['DNI'];
+                $_SESSION['telefono'] = $user_data['telefono'];
+                $_SESSION['fechanato'] = $user_data['fechanato'];
+                $_SESSION['email'] = $user_data['email'];
             }
-            unset($_SESSION["token"]);
-        } 
-} 
+        }
+    }
+}else{
+    exit("Fallo en la validacion del token CSRF");
+}
