ff7293e5e37f1c43a8a61ee48a5660fafc87984f
Foxo10 <oiertxu.ten@gmail.com>
2022-11-20 22:37:02 +0100

    csrdf

--- app/csrf.php
+++ app/csrf.php
@@ -1,30 +1,17 @@
 <?php
 
 session_start();
-if(isset($_POST) & !empty($_POST)){
-    //Validacion token CSRF
-    if($_POST['csrf_token']== $_SESSION['csrf_token']){
-        echo "token CSRF validado";
-    } else{
-        echo "Problemas de validacion con el token CSRF";
-    }
-}
-// tiempo de validacion
-$max_time = 60*60*24;
-if(isset($_SESSION['csrf_token_time'])){
-    $token_time = $_SESSION['csrf_token_time'];
-    if(($token_time + $max_time) >= time()){
-        unset($_SESSION['csrf_token']);
-        unset($_SESSION['csrf_token_time']);
-    }
-}
-    
 
-//1. crear token
-$token = md5(uniqid(rand(), true));
-$_SESSION['csrf_token'] = $token;
-$_SESSION['csrf_token_time'] = time();
-//2. añadir token al formulario
-//<input type="hidden" name="csrf_token" value="<?php echo $token;
+class CSRF {
+    public static function create_token(){
+        $token = md5(time());
+        $_SESSION['token'] = $token;
+
+        echo "input name='token' value='$token' type='hidden'>";
+    }
+    public static function validate($token){
+        return isset($_SESSION['token']) && $_SESSION['token'] == $token;
+    }
+}
 
 ?> 
