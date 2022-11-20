ddc39cbbed14189052100016055318b90f4ccb7e
Foxo10 <oiertxu.ten@gmail.com>
2022-11-20 19:05:25 +0100

    sdfsadfasdf

--- app/index.php
+++ app/index.php
@@ -1,6 +1,7 @@
 <?php session_start();
 header("X-Frame-Options: SAMEORIGIN");
-header("X-Content-Type-Options: nosniff");?>
+header("X-Content-Type-Options: nosniff");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
